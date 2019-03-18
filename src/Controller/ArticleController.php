462a47ee47b425811bd3c3135add7da21a90f94a
mayoctt@gmail.com <46900928+asilingas@users.noreply.github.com>
2019-03-18 13:37:40 +0200

    start

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: new
+ * Date: 2019.03.18
+ * Time: 13:25
+ */
+
+namespace App\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class ArticleController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('MY first symfony page!');
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+        return new Response(sprintf(
+            'Future page to show the article: %s',
+            $slug));
+    }
+}
